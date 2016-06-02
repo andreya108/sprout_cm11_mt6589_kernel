1b27a7a777e5923a85a5ef63c2d3630cd4bf1379
Andrey Alekseenko <andrei.alekseenko@gmail.com>
2016-06-02 04:45:16 +0300

    added support for gcc5,6,7

--- mk.sh
+++ mk.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Default settings
 
-PATH=$PWD/prebuilts/arm-linux-androideabi-4.7/bin:$PATH
+export PATH=$PWD/toolchains/UBERTC/DespairFactor-arm-eabi-6.0/bin:$PATH
+export CROSS_COMPILE=arm-eabi-
+#PATH=$PWD/prebuilts/arm-linux-androideabi-4.7/bin:$PATH
 
 make $1 $2
